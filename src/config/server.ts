4233225e2c234bd1d99b0441bca7ef258a812299
ianil91 <019mepa@gmail.com>
2020-09-15 03:20:45 +0530

    versions added

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,6 +1,19 @@
 import express from 'express';
+import { parse } from './versions/v1';
+import { parseV2 } from './versions/v2';
 const app = express()
 const port : string|number= process.env.PORT || 3000;
+app.use(express.json());
+
+app.post('/api/v1/parse', function (req, res) {
+   res.send(parse(req.body));
+})
+
+app.post('/api/v2/parse', function (req, res) {
+   res.send(parseV2(req.body));
+})
+
 app.use("*",(req, res) =>{res.send("<h1>Welcome to your server!</h1>")})
-//create a server object:
+
+
 app.listen(port,() => console.log(`hosting @${port}`));
---
+++ src/config/versions/v2.ts
@@ -0,0 +1,33 @@
+export const parseV2 = (reqObj: any) => {
+    try {
+    let firstName = '';
+    let lastName = '';
+    let clientId = '';
+    let tempCharArray = [];
+    let textString = reqObj && reqObj.data;
+    for (let a = 0; a < textString.length; a++) {
+        if(textString[a]!== '0') {
+            tempCharArray.push(textString[a])
+        } else {
+            if (!firstName) {
+                firstName = tempCharArray.join('');
+            } else if (!lastName) {
+                lastName = tempCharArray.join('');
+            }
+            tempCharArray = [];
+        }
+    }
+    if (!clientId) {
+        clientId = tempCharArray.join('');
+        clientId = `${clientId.substring(0,3)}-${clientId.substring(3,clientId.length)}`
+    }
+    return {
+        firstName,
+        lastName,
+        clientId,
+    };
+    } catch (err) {
+        throw new Error('something bad happened');
+    }
+    
+}
